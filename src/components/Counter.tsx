9bd35d2af80ccee9122642013587625d5427bb56
eobcre <eobcre@gmail.com>
2023-09-17 16:03:10 -0400

    created counter logic

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+
 const Counter: React.FC = () => {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+
+  const handleClickPlus = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleClickMinus = () => {
+    count > 0 ? setCount((prev) => prev - 1) : '';
+  };
+
+  return (
+    <div>
+      <button onClick={handleClickMinus}>-</button>
+      <p>{count}</p>
+      <button onClick={handleClickPlus}>+</button>
+    </div>
+  );
 };
 
 export default Counter;
