58681b4964300341ebada76e8e5aa55cffcd1008
eobcre <eobcre@gmail.com>
2023-09-17 16:06:28 -0400

    created price summary

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,5 +1,17 @@
 const Price: React.FC = () => {
-  return <div>Price</div>;
+  return (
+    <div>
+      <div>
+        <p>Starndard Package</p>
+        <p>Free</p>
+      </div>
+      <div>
+        <p>Total USD</p>
+        <p>$399.99</p>
+      </div>
+      <a href='#'>Got a gift card or a promotional code?</a>
+    </div>
+  );
 };
 
 export default Price;
