b79b4d929b807c9463a5012021d7764e3b9e6cbc
eobcre <eobcre@gmail.com>
2023-09-17 16:04:08 -0400

    created cart item, no image yet

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,5 +1,11 @@
 const CartItem: React.FC = () => {
-  return <div>CartItem</div>;
+  return (
+    <div>
+      <img src='' alt='' />
+      <p>Ecovacs Deebot OZMO N8 + robot vacuum cleaner W125978483</p>
+      <span>In stock</span>
+    </div>
+  );
 };
 
 export default CartItem;
