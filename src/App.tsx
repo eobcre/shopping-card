c009e2f714cd8973840925ebad61d5c2510d3e20
eobcre <eobcre@gmail.com>
2023-09-17 15:00:34 -0400

    placed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import Title from './components/Title';
+import CartItem from './components/CartItem';
+import Counter from './components/Counter';
+import Price from './components/Price';
+import Button from './components/Button';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Title />
+        <CartItem />
+        <Counter />
+        <Price />
+        <Button />
+      </div>
+    </div>
+  );
 };
 
 export default App;
