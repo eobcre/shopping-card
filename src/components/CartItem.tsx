9931697ed464ceffaac7e4744ad186c295094b70
eobcre <eobcre@gmail.com>
2023-09-17 16:28:38 -0400

    updated layout to style, new image

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,8 +1,14 @@
+import Image from '../assets/item.png';
+
 const CartItem: React.FC = () => {
   return (
     <div>
-      <img src='' alt='' />
-      <p>Ecovacs Deebot OZMO N8 + robot vacuum cleaner W125978483</p>
+      <div className='item-container'>
+        <img src={Image} alt='Image' width={70} height={70} />
+        <p className='item-name'>
+          Ecovacs Deebot OZMO N8 + robot vacuum cleaner W125978483
+        </p>
+      </div>
       <span>In stock</span>
     </div>
   );
